637631c01b3f9fbd8b9a9049426602f9b20dd5e7
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2023-07-17 19:13:56 +0530

    Add files via upload

---
+++ circular_linkedlist_traversal.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+void linkedlisttraversal(struct node *head){
+    struct node *ptr = head;
+    do{
+        printf("element is %d\n",ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
+}
+
+int main(){
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    //Allocate memory for node in the linked list in help
+    head   = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third  = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    //link the first to second node
+    head->data=4;
+    head->next=second;
+
+    //link the second and third node
+    second->data=3;
+    second->next=third;
+
+    //link the third and fourth node
+    third->data=6;
+    third->next=fourth;
+
+    //terminate the list at the third node
+    fourth->data=1;
+    fourth->next=head;
+
+    linkedlisttraversal(head);
+    return 0;
+
+}
